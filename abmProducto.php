53ccc3dbf8d0f5bf552e69c8ee742b86b852a59c
12jmad <55392943+12jmad@users.noreply.github.com>
2020-01-23 07:52:21 -0300

    cambios

--- abmProducto.php
+++ abmProducto.php
@@ -1,6 +1,8 @@
 <?php
 require_once ('includes/pdo.php');
+require_once 'clases/Conexion.php';
 require_once 'clases/Producto.php';
+
 
 $producto = new Producto();
 function obtenerListaProductos($db){
@@ -21,7 +23,8 @@ function obtenerListaProductos($db){
   }
   
 }
-function obtenerListaMarcas($db){
+function obtenerListaMarcas(){
+  $db=Conexion::conectar();
   try {
     $sql = "SELECT id_marca,marca 
       FROM marcas";
@@ -35,7 +38,9 @@ function obtenerListaMarcas($db){
     $e->getMessage();
   }  
 }
-function obtenerListaCategorias($db){
+function obtenerListaCategorias(){
+  $db=Conexion::conectar();
+
   try {
     $sql = "SELECT id_categoria,categoria
       FROM categorias";
@@ -49,7 +54,7 @@ function obtenerListaCategorias($db){
     $e->getMessage();
   }
 }
-$variable=obtenerListaProductos($db);
+$variable=$producto->obtenerListaProductos($db);
 //var_dump($variable);
 if ($_POST) {
  // var_dump($_POST);
@@ -64,11 +69,11 @@ if ($_POST) {
     $descuento = $_POST["descuento"];
     $img = "img/productos/phone.jpg";//$_POST["img"]; esto lo deje asi para que funciones pero tendria que ir la direccion
 
-    $producto->altaProducto($db, $nombre, $descripcion,$precio, $stock, $marca, $categoria, $descuento, $img);
+    $producto->altaProducto($img);
   }elseif (isset($_POST["btnBorrar"])) {
     $id = $_POST["id"];
 
-    $producto->borrarProducto($db, $id);
+    $producto->borrarProducto($id);
   }
   if (isset($_POST["modificar_id"])) {
     $id=2;//de alguna manera le tiene que llegar un id 
@@ -80,7 +85,7 @@ if ($_POST) {
     $categoria = $_POST["categoria"];
     $descuento = $_POST["descuento"];
     $img = "img/productos/phone.jpg";
-    $producto->modificarProducto($db,$id,$nombre, $descripcion,$precio, $stock, $marca, $categoria, $descuento, $img);
+    $producto->modificarProducto($id,$nombre, $descripcion,$precio, $stock, $marca, $categoria, $descuento, $img);
   }
 }
 ?>
@@ -120,77 +125,11 @@ if ($_POST) {
                 </div>
                 <div class="form-group">
                   <label for="precio">Precio:</label>
-                  <input type="number" class="form-control" id="precio" name="precio" min="0">
+                  <input type="number" class="form-control " id="precio" name="precio" min="0" value="0" >
                 </div>
                 <div class="form-group">
                   <label for="stock">stock</label>
-                  <input type="number" class="form-control" id="stock" name="stock" min="0">
-                </div>
-                <div class="form-group">
-                  <label for="marca">Marca</label>
-                  <select class="form-control" id="marca" name="marca">
-                    <?php 
-                      $marcas=obtenerListaMarcas($db);
-                      foreach ($marcas as $key => $value) { 
-                    ?>
-                      <option value="<?=$value["id_marca"];?>"><?=$value["marca"];?></option>
-                    <?php 
-                      } 
-                    ?>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="categoria">Categoria</label>
-                  <select class="form-control" id="categoria" name="categoria">
-                  <?php 
-                      $marcas=obtenerListaCategorias($db);
-                      foreach ($marcas as $key => $value) { 
-                    ?>
-                      <option value="<?=$value["id_categoria"];?>"><?=$value["categoria"];?></option>
-                    <?php 
-                      } 
-                    ?>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="descuento">descuento</label>
-                  <input type="number" class="form-control" id="descuento" name="descuento" min="0" max="100">
-                </div>
-                <div class="form-group">
-                  <label for="img">Imagen</label>
-                  <input type="file" class="form-control-file" id="img" name="img">
-                </div>
-                <button type="submit" class="btn btn-primary mb-2" name="btnCargar" value="cargar">Cargar</button>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Modificar Productos
-              </button>
-            </h5>
-          </div>
-          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body">
-              <form class="modificarProducto" action="" method="post">
-                <div class="form-group">
-                  <label for="nombre">Nombre</label>
-                  <input type="text" class="form-control" id="nombre" name="nombre">
-                </div>
-                <div class="form-group">
-                  <label for="descripcion">Descripcion</label>
-                  <textarea class="form-control" id="descripcion" rows="8" cols="80" name="descripcion"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="precio">Precio:</label>
-                  <input type="number" class="form-control" id="precio" name="precio" min="0">
-                </div>
-                <div class="form-group">
-                  <label for="stock">stock</label>
-                  <input type="number" class="form-control" id="stock" name="stock" min="0">
+                  <input type="number" class="form-control" id="stock" name="stock" min="0" value="0">
                 </div>
                 <div class="form-group">
                   <label for="marca">Marca</label>
@@ -221,6 +160,72 @@ if ($_POST) {
                 <div class="form-group">
                   <label for="descuento">descuento</label>
                   <input type="number" class="form-control" id="descuento" name="descuento" min="0" max="100" value="0">
+                </div>
+                <div class="form-group">
+                  <label for="img">Imagen</label>
+                  <input type="file" class="form-control-file" id="img" name="img">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2" name="btnCargar" value="cargar">Cargar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Modificar Productos
+              </button>
+            </h5>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+              <form class="modificarProducto" action="modificarProducto.php" method="post">
+                <div class="form-group">
+                  <label for="nombre">Nombre</label>
+                  <input type="text" class="form-control" id="nombre" name="nombre" value="">
+                </div>
+                <div class="form-group">
+                  <label for="descripcion">Descripcion</label>
+                  <textarea class="form-control" id="descripcion" rows="8" cols="80" name="descripcion"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="precio">Precio:</label>
+                  <input type="number" class="form-control" id="precio" name="precio" min="0" value="">
+                </div>
+                <div class="form-group">
+                  <label for="stock">stock</label>
+                  <input type="number" class="form-control" id="stock" name="stock" min="0" value="">
+                </div>
+                <div class="form-group">
+                  <label for="marca">Marca</label>
+                  <select class="form-control" id="marca" name="marca">
+                    <?php 
+                      $marcas=obtenerListaMarcas($db);
+                      foreach ($marcas as $key => $value) { 
+                    ?>
+                      <option value="<?=$value["id_marca"];?>"><?=$value["marca"];?></option>
+                    <?php 
+                      } 
+                    ?>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="categoria">Categoria</label>
+                  <select class="form-control" id="categoria" name="categoria">
+                  <?php 
+                      $marcas=obtenerListaCategorias($db);
+                      foreach ($marcas as $key => $value) { 
+                    ?>
+                      <option value="<?=$value["id_categoria"];?>"><?=$value["categoria"];?></option>
+                    <?php 
+                      } 
+                    ?>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="descuento">descuento</label>
+                  <input type="number" class="form-control" id="descuento" name="descuento" min="0" max="100" value="">
                 </div>
                 <div class="form-group">
                   <label for="img">Imagen</label>
@@ -262,79 +267,79 @@ if ($_POST) {
           <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
             <ul class="list-group">
               <li class="list-group-item">
-                <div class="card-body form-inline px-0">
+                <div class="card-body form-inline d-flex justify-content-between px-0">
                   <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Id</span>
-                  </div>
-                  <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Nombre</span>
+                      <span  class="form-control-plaintext text-center">Id</span>
                   </div>
                   <div class="form-group mb-1 col-2 px-1" >
-                      <span for="" class="form-control-plaintext">Descripcion</span>
+                      <span  class="form-control-plaintext text-center">Nombre</span>
                   </div>
                   <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Precio</span>
+                      <span  class="form-control-plaintext text-center">Descripcion</span>
+                  </div>
+                  <div class="form-group mb-1 col-1 " >
+                      <span  class="form-control-plaintext text-center">Precio</span>
                   </div>
                   <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Stock</span>
-                  </div>
-                  <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Marca</span>
-                  </div>
-                  <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="form-control-plaintext">Categoria</span>
-                  </div>
-                  <div class="form-group mb-1 col-1 px-1" >
-                      <span for="" class="d-block form-control-plaintext">Descuento</span>
+                      <span  class="form-control-plaintext text-center">Stock</span>
                   </div>
                   <div class="form-group mb-1 col-2 px-1" >
-                      <span for="" class="d-block text-center form-control-plaintext">Imagen</span>
+                      <span  class="form-control-plaintext text-center">Marca</span>
+                  </div>
+                  <div class="form-group mb-1 col-1 px-1" >
+                      <span  class="form-control-plaintext text-center">Categoria</span>
+                  </div>
+                  <div class="form-group mb-1 col-1 px-1" >
+                      <span  class="d-block form-control-plaintext text-center">Descuento</span>
+                  </div>
+                  <div class="form-group mb-1 col-2 px-1" >
+                      <span  class="d-block text-center form-control-plaintext text-center">Imagen</span>
                   </div>
                 </div>
               </li>
               <?php foreach ($variable as $key => $value) { ?>
 
                 <li class="list-group-item">
-                  <div class="card-body px-0">
-                    <form class="form-inline" action="" method="post">
+                  <div class="card-body d-flex justify-content-between px-0">
+                    <form class="form-inline" action="modificarProducto.php" method="post">
                       <div class="form-group mb-1 col-1 px-1" >
-                        <input type="text" readonly class="form-control-plaintext" id="id" value="<?=$value->getId();?>" name="id">
+                        <input type="text" readonly class="form-control-plaintext text-center" id="id" value="<?=$value->getId();?>" name="id">
                       </div>
-                      <div class="form-group mb-2 col-1 px-1">
+                      <div class="form-group mb-2 col-2 px-1">
 
-                        <input type="text"  class="form-control-plaintext" id="nombre" value="<?=$value->getNombre();?>" name="nombre">
-                      </div>
-                      <div class="form-group mb-2 col-2">
-
-                        <input type="text" class="form-control-plaintext" name="descripcion" readonly  id="descripcion" value="<?=$value->getDescripcion();?>">
+                        <span  class="form-control-plaintext text-center" ><?=$value->getNombre();?></span>
                       </div>
                       <div class="form-group mb-2 col-1">
 
-                        <input type="text" class="form-control-plaintext" name="precio" readonly  id="precio" value="$ <?=$value->getPrecio();?>">
+                        <span class="form-control-plaintext text-center" >Aqui</span>
+                      </div>
+                      <div class="form-group mb-2 col-1">
+
+                        <span class="form-control-plaintext text-center" >$ <?=$value->getPrecio();?></span>
                       </div>
                       <div class="form-group mb-2 col-1 px-1">
 
-                        <input type="text" class="form-control-plaintext" name="stock" value="<?=$value->getStock();?>" id="stock">
+                        <span class="form-control-plaintext text-center" ><?=$value->getStock();?></span>
+                      </div>
+                      <div class="form-group mb-2 col-2 px-1">
+
+                        <span class="form-control-plaintext text-center" ><?=$value->getMarca();?></span>
                       </div>
                       <div class="form-group mb-2 col-1 px-1">
 
-                        <input type="text" class="form-control-plaintext" name="marca" value="<?=$value->getMarca();?>" id="marca">
+                        <span class="form-control-plaintext text-center" ><?=$value->getCategoria();?></span>
                       </div>
                       <div class="form-group mb-2 col-1 px-1">
 
-                        <input type="text" class="form-control-plaintext" name="categoria" value="<?=$value->getCategoria();?>" id="categoria">
+                        <span class="form-control-plaintext text-center" ><?=$value->getDescuento();?>%</span>
                       </div>
-                      <div class="form-group mb-2 col-1 px-1">
-
-                        <input type="text" class="form-control-plaintext" name="descuento" value="<?=$value->getDescuento();?>" id="categoria">
-                      </div>
-                      <div class="form-group mb-2 col-2">
+                      <div class="form-group mb-2 col-2 text-center">
 
                         <img src="<?=$value->getImg();?>" alt="" sizes="30px">
                       </div>
-                      <div class="form-group mb-2 col-4">
-                        <button type="submit" class="btn btn-primary mx-2 mb-1 " name="modificar_l" value="<?=$key;?>">Modificar</button>
-                        <button type="submit" class="btn btn-primary mx-2 mb-1 " name="eliminar_l" value="<?=$key;?>">Eliminar</button>
+                      <div class="form-group mb-2 col-12 text-center">
+                        <button type="submit" class="btn btn-primary mx-2 mb-1 " name="modificar_l" value="<?=$value->getId();?>">Modificar</button>
+                        <button type="submit" class="btn btn-primary mx-2 mb-1 " name="eliminar_l" value="<?=$value->getId();?>">Eliminar</button>
                       </div>
                     </form>
                   </div>
