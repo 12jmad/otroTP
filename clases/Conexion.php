cde0198370a6b13fe57f8820093684b16a258e25
12jmad <55392943+12jmad@users.noreply.github.com>
2020-01-23 07:50:57 -0300

    cambios

---
+++ clases/Conexion.php
@@ -0,0 +1,24 @@
+<?php
+class Conexion{
+    static function conectar(){
+        $dsn = "mysql:host=localhost;dbname=e-commerce";
+        $user = "root";
+        $pass = "";
+
+        try
+        {
+        $db = new PDO($dsn, $user,$pass);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->exec('SET CHARACTER SET utf8');
+        return $db;
+        }
+        catch (\Exception $e)
+        {
+        
+        $e->getMessage();
+        echo "Error al conectar la base de datos";
+        return false;
+        }
+    }
+}
+?>
