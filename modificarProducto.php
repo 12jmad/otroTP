1245ca4be3dd550d85cd7e5ac15dbc7de922b0b8
12jmad <55392943+12jmad@users.noreply.github.com>
2020-01-24 17:36:03 -0300

    Update modificarProducto.php

--- modificarProducto.php
+++ modificarProducto.php
@@ -1,70 +1,39 @@
 <?php
-require_once ('includes/pdo.php');
-require_once 'clases/Conexion.php';
-require_once 'clases/Producto.php';
-if(isset($_POST["id"])){
-    $producto = new Producto();
-    $id=(int)$_POST["modificar_l"];
-    $unProducto=$producto->buscarPorId($id);
-    
-}
-var_dump($id);
-function obtenerListaMarcas(){
-  $db=Conexion::conectar();
-  try {
-    $sql = "SELECT id_marca,marca 
-      FROM marcas";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $variable = $stmt->fetchAll(PDO::FETCH_ASSOC);//array asociado
-    $stmt->closeCursor();
-    return $variable;  
-  } catch (\Exception $e) {
-    echo "Error al obtener Lista de Marcas";
-    $e->getMessage();
-  }  
-}
-function obtenerListaCategorias(){
-  $db=Conexion::conectar();
 
-  try {
-    $sql = "SELECT id_categoria,categoria
-      FROM categorias";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $variable = $stmt->fetchAll(PDO::FETCH_ASSOC);//array asociado
-    $stmt->closeCursor();
-    return $variable;
-  } catch (\Exception $e) {
-    echo "Error al obtener Lista de Categorias";
-    $e->getMessage();
+function guardarArchivo($file,$nombre="text"){
+  if($file["name"]!=""){
+      $nombreArchivo=$file["name"];
+  $archivo=$file["tmp_name"];
+  $ext=pathinfo($nombreArchivo,PATHINFO_EXTENSION);
+  $miArchivo="img/productos/".$nombre;
+   //ruta actual
+  $nombre="phone".uniqid();
+  if (!file_exists($miArchivo)) {
+      mkdir($miArchivo, 0777, true);
   }
+  $directorio = opendir($miArchivo);
+  $cont=0;
+  $archivoEliminar="";
+  while ($archivoDir = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
+  {
+      if (!is_dir($archivoDir))//verificamos si es o no un directorio
+          {
+      //var_dump ($archivoDir );
+          if(preg_match("/phone/i" ,$archivoDir)){//encuentra un archivo que coincida con el patron
+              $cont++;
+              $archivoEliminar=$archivoDir;   
+          }
+      }
+  }
+  if($cont>1){
+      unlink($miArchivo."/".$archivoEliminar);
+  }
+  $miArchivo=$miArchivo."/".$nombre.".".$ext;
+  move_uploaded_file($archivo,$miArchivo);
+  return $miArchivo;
+  }
+  return null;
 }
-
-if (isset($_POST["modificar_id"])&& $_POST ){
-    $id=$unProducto->getId();//de alguna manera le tiene que llegar un id 
-    $nombre = $_POST["nombre"];
-    $descripcion = $_POST["descripcion"];
-    $precio= $_POST["precio"];
-    $stock = $_POST["stock"];
-    $marca = $_POST["marca"];
-    $categoria = $_POST["categoria"];
-    $descuento = $_POST["descuento"];
-    $img = "img/productos/phone.jpg";
-    $producto->modificarProducto($id,$nombre, $descripcion,$precio, $stock, $marca, $categoria, $descuento, $img);
-}
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<?php include 'includes/head.php';?>
-<title>ABM Productos</title>
-
-<body>
-
-  <?php include 'includes/headerAdm.php'; ?>
-
-<?php
-require_once ('includes/pdo.php');
 require_once 'clases/Conexion.php';
 require_once 'clases/Producto.php';
 echo "<pre>";
@@ -110,11 +79,16 @@ function obtenerListaCategorias(){
 if (isset($_POST["modificar_id"])&& $_POST ){
   
     $id= ((int)$_POST["idM"]);//de alguna manera le tiene que llegar un id 
-
-    $img = "img/productos/phone.jpg";
+    if($_FILES){
+      $img=guardarArchivo($_FILES["img"],$_POST["nombre"]);  
+    }else{
+      $img=$_POST["imagenActual"];
+    }
+    
+    //var_dump($imagen);
+   // $img = "img/productos/phone.jpg";
     $producto->modificarProducto($id,$img);
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -125,6 +99,8 @@ if (isset($_POST["modificar_id"])&& $_POST ){
 
   <?php include 'includes/headerAdm.php'; ?>
 
+ 
+
   <main class="mb-3">
     
   <div class="container bg-white">
@@ -133,7 +109,7 @@ if (isset($_POST["modificar_id"])&& $_POST ){
             <a href="abmProducto.php" class="btn btn-primary col-2 py-3">Volver Atras</a>
         </div>
     <div class="card-body">
-              <form class="modificarProducto" action="" method="post">
+              <form class="modificarProducto" action="" method="post" enctype="multipart/form-data">
               <input type="number" class="form-control" id="id" name="idM" value="<?=$unProducto->getId();?>" readonly hidden>
                 <div class="form-group">
                   <label for="nombre">Nombre</label>
@@ -186,9 +162,12 @@ if (isset($_POST["modificar_id"])&& $_POST ){
                   <label for="descuento">descuento</label>
                   <input type="number" class="form-control" id="descuento" name="descuento" min="0" max="100" value="<?=$unProducto->getDescuento();?>">
                 </div>
-                <div class="form-group">
-                  <label for="img">Imagen</label>
-                  <input type="file" class="form-control-file" id="img" name="img">
+                <div class="form-group text-center">
+                  <label for="img" class="col-12">Imagen</label>
+                  <img src="<?=$unProducto->getImg();?>" alt="" sizes="" width="100px" class="col-5">
+                  <input type="text" name="imagenActual" value="<?=$unProducto->getImg();?>" readonly hidden>
+                  <label for="img" class="text-left col-12">Cambiar</label>
+                  <input type="file" class="form-control-file" id="img" name="img" class="">
                 </div>
                 <button type="submit" class="btn btn-primary mb-2" name="modificar_id" value="">Modificar</button>
               </form>
